1827c87956040e894022a4c93cf1d37aadd42a25
Marius Brt <marius.brouty@gmail.com>
2022-12-12 22:36:24 +0100

    resize images

---
+++ format_imgs.py
@@ -0,0 +1,12 @@
+import os
+from PIL import Image
+
+for f in os.listdir("./imgs"):
+    basewidth = 256
+    img = Image.open("./imgs/" + f)
+    if img.size[0] == basewidth:
+        continue
+    wpercent = basewidth / float(img.size[0])
+    hsize = int((float(img.size[1]) * float(wpercent)))
+    img = img.resize((basewidth, hsize), Image.Resampling.LANCZOS)
+    img.save("./imgs/" + f)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import github
 import os
 import json
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,9 +9,33 @@ g = github.Github(os.getenv("TOKEN"))
 
 repo = g.get_user().get_repo("dev-tools-data")
 
-versions = repo.get_contents("./versions.json")
-print(json.loads(file.decoded_content.decode("utf-8")))
+file = repo.get_contents("./packages.json")
+versionsFile = repo.get_contents("./versions.json")
+pkgs = json.loads(file.decoded_content.decode("utf-8"))
+versions = json.loads(versionsFile.decoded_content.decode("utf-8"))
 
-""" repo.update_file(
-    "./versions.json", "bot test commit", json.dumps(obj, indent=4), file.sha
-) """
+for pkg in pkgs:
+    x = requests.get(
+        "https://api.github.com/repos/microsoft/winget-pkgs/contents/manifests/"
+        + pkg["winget"][0].lower()
+        + "/"
+        + pkg["winget"].split(".")[0]
+        + "/"
+        + pkg["winget"].split(".")[1]
+    )
+    if x.status_code == 200:
+        versions[pkg["name"]] = []
+        vrs = json.loads(x.content.decode("utf-8"))
+        for vr in vrs:
+            if vr["name"].replace(".", "").isnumeric():
+                versions[pkg["name"]].append(vr["name"])
+
+for pkg in versions:
+    versions[pkg] = sorted(versions[pkg], key=lambda x: [int(i) for i in x.split(".")])
+
+repo.update_file(
+    "./versions.json",
+    "Update versions",
+    json.dumps(versions, separators=(",", ":")),
+    versionsFile.sha,
+)
