8bc4adeb70e312ae4c25c981e0071b29a4c34db9
Marius Brt <marius.brouty@gmail.com>
2022-12-12 21:54:08 +0100

    packages file

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import github
 import os
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
 g = github.Github(os.getenv("TOKEN"))
 
 repo = g.get_user().get_repo("dev-tools-data")
-file = repo.get_file_contents("/versions.json")
 
-repo.update_file("/versions.json", "bot test commit", "hello world!", file.sha)
+versions = repo.get_contents("./versions.json")
+print(json.loads(file.decoded_content.decode("utf-8")))
+
+""" repo.update_file(
+    "./versions.json", "bot test commit", json.dumps(obj, indent=4), file.sha
+) """
